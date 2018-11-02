b0eabc04c9969362d7cbc3229f635b95576af10c
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-02 21:30:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence) 
-  sen = []
    sentence = sentence.split(" ")
     sentence.collect do |i|
-     sen << i.reverse! 
+      i.reverse! 
     end
-    sen.join(" ")
+    
 end
